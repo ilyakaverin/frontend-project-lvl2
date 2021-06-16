19496fdd1a17154916ef614b06027c83963bef02
ikaverin <kaverin1990@gmail.com>
2021-06-16 14:51:35 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,8 +2,8 @@
 import program from 'commander';
 
 program
-.version('1.0.0')
-.description('Compares two configuration files and shows a difference.')
-.option('-f, --format','output format')
-.arguments('<filepath1> <filepath2>')
-program.parse() 
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format', 'output format')
+  .arguments('<filepath1> <filepath2>');
+program.parse();
