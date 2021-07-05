47637a8bfcaeb4ef9602b97c8f092d407cc78485
ikaverin <kaverin1990@gmail.com>
2021-07-05 19:54:01 +0300

    one more fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,5 +10,5 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 test('step4', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(readFile('expectedFile.json'));
+  expect(genDiff('before.json', 'after.json')).toEqual(readFile('expectedFile.json'));
 });
